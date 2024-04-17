1c930284e2eb51147ada5b81d6050b89df188d38
Diamond Alexander <diamondalexander@users.noreply.github.com>
2024-04-16 20:57:47 -0700

    Added passthrough for images, favicons, and assets

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,9 @@
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./src/css");
     eleventyConfig.addWatchTarget("./src/css/");
+    eleventyConfig.addPassthroughCopy("./src/images/");
+    eleventyConfig.addPassthroughCopy("./src/assets/");
+    eleventyConfig.addPassthroughCopy({ "./src/favicons": "/" });
     return {
       dir: {
         input: "src",
