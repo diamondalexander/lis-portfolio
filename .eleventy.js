aa0503698b69acd25c8ebcadf6a25782edc9f41d
Diamond Alexander <diamondalexander@users.noreply.github.com>
2024-04-17 21:48:16 -0700

    Add pathPrefix and baseHTML transformation support - for GitHub repository subdirectory structure

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,5 @@
+const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
+
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./src/css");
     eleventyConfig.addWatchTarget("./src/css/");
@@ -5,7 +7,9 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./src/assets/");
     eleventyConfig.addPassthroughCopy("./src/lis-portfolio/assets/");
     eleventyConfig.addPassthroughCopy({ "./src/favicons": "/" });
+    eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
     return {
+      pathPrefix: "/lis-portfolio/",
       dir: {
         input: "src",
         output: "public",
