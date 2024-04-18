361a0ad1457b1e57a7c42edbcdadd2e232c9087f
Diamond Alexander <diamondalexander@users.noreply.github.com>
2024-04-17 17:49:20 -0700

    Add passthroughcopy for lis-portfolio folder (to work with repository name structure in GitHub pages)

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addWatchTarget("./src/css/");
     eleventyConfig.addPassthroughCopy("./src/images/");
     eleventyConfig.addPassthroughCopy("./src/assets/");
+    eleventyConfig.addPassthroughCopy("./src/lis-portfolio/assets/");
     eleventyConfig.addPassthroughCopy({ "./src/favicons": "/" });
     return {
       dir: {
