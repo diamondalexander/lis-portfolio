62609ffc00318da3315cf26794d59cf556c079e5
Diamond Alexander <diamondalexander@users.noreply.github.com>
2024-04-17 22:01:19 -0700

    Remove lis-portfolio directory and files

--- .eleventy.js
+++ .eleventy.js
@@ -5,7 +5,6 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addWatchTarget("./src/css/");
     eleventyConfig.addPassthroughCopy("./src/images/");
     eleventyConfig.addPassthroughCopy("./src/assets/");
-    eleventyConfig.addPassthroughCopy("./src/lis-portfolio/assets/");
     eleventyConfig.addPassthroughCopy({ "./src/favicons": "/" });
     eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
     return {
